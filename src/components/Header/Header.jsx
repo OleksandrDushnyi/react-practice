a6c8b9be3503bcc367bf1509dde02966192cb2c2
Sanyok8 <129120542+OleksandrDushnyi@users.noreply.github.com>
2024-02-14 00:00:13 +0200

    update

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import logo from '/logo-name.svg'
+import {styled} from 'styled-components'
+
+
+const HeaderContainer = styled.header`
+height: 50px;
+display: flex;
+padding: 0 2rem;
+justify-content: space-between;
+align-items: center;
+border-bottom: 1px solid #ccc;
+background: #fafafa;
+
+`
+
+export default function Header() {
+   const [now, setNow] = useState(new Date())
+   setInterval(() => setNow(new Date()), 1000)
+    return(
+      <HeaderContainer>
+        <img src={logo} alt="Result" />
+      {/* <h3>RESULT</h3> */}
+      <span>Time now: {now.toLocaleTimeString()}</span>
+      </HeaderContainer>
+    )
+  }
+  
