ab1a418ab35392e55cec8b2b6732b7226581cc7f
Oleksandr Dushnyi <sdusnij@gmail.com>
2024-04-30 00:18:21 +0300

    update users-find App

--- users-find App/src/components/Users/CreateUser.js
+++ users-find App/src/components/Users/CreateUser.js
@@ -1,24 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import styles from "./CreateUser.module.css";
 import ErrorModal from "../UI/ErrorModal";
 
 const CreateUser = (props) => {
-  const [inputName, setInputName] = useState("");
-  const [inputAge, setInputAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  // const [inputName, setInputName] = useState("");
+  // const [inputAge, setInputAge] = useState("");
   const [error, setError] = useState();
 
   const createUserHandler = (event) => {
     event.preventDefault();
-    if (inputName.trim().length === 0 || inputAge.trim().length === 0) {
+    const inputUserName = nameInputRef.current.value;
+    const inputUserAge = ageInputRef.current.value;
+    if (inputUserName.trim().length === 0 || inputUserAge.trim().length === 0) {
       setError({
         title: "Неправильний ввід даних",
         message: "Ці поля не можуть бути пустими",
       });
       return;
     }
-    if (+inputAge < 1) {
+    if (+inputUserAge < 1) {
       setError({
         title: "Неправильний вік",
         message: "Вік повинен бути більшим за 0",
@@ -26,17 +31,17 @@ const CreateUser = (props) => {
       return;
     }
     // console.log(inputName, inputAge);
-    props.onCreateUser(inputName, inputAge);
-    setInputName("");
-    setInputAge("");
+    props.onCreateUser(inputUserName, inputUserAge);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
-  const nameChangeHandler = (event) => {
-    setInputName(event.target.value);
-  };
-  const ageChangeHandler = (event) => {
-    setInputAge(event.target.value);
-  };
+  // const nameChangeHandler = (event) => {
+  //   setInputName(event.target.value);
+  // };
+  // const ageChangeHandler = (event) => {
+  //   setInputAge(event.target.value);
+  // };
 
   const errorHandler = () => {
     setError(false);
@@ -54,19 +59,9 @@ const CreateUser = (props) => {
       <Card className={styles.input}>
         <form onSubmit={createUserHandler}>
           <label htmlFor="name">Ім'я</label>
-          <input
-            id="name"
-            type="text"
-            onChange={nameChangeHandler}
-            value={inputName}
-          ></input>
+          <input id="name" type="text" ref={nameInputRef}></input>
           <label htmlFor="age">Вік</label>
-          <input
-            id="age"
-            type="text"
-            onChange={ageChangeHandler}
-            value={inputAge}
-          ></input>
+          <input id="age" type="text" ref={ageInputRef}></input>
           <Button type="submit">Добавити користувача</Button>
         </form>
       </Card>
